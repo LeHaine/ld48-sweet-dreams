e4575eb3982796b7168b33667ae9f2dcb338716e
LeHaine <colt.daily@gmail.com>
2021-04-23 21:58:40 -0400

    update hero movement logic

--- src/commonMain/kotlin/com/lehaine/game/Hero.kt
+++ src/commonMain/kotlin/com/lehaine/game/Hero.kt
@@ -44,6 +44,7 @@ class Hero(
 
     companion object {
         const val ON_GROUND_RECENTLY = "onGroundRecently"
+        const val JUMP_EXTRA = "jumpExtra"
     }
 
     private sealed class HeroState {
@@ -51,22 +52,53 @@ class Hero(
         object Run : HeroState()
         object Jump : HeroState()
         object DoubleJump : HeroState()
+        object Fall : HeroState()
     }
 
-    private val moveSpeed = 0.05
+    private val moveSpeed = 0.03
     private val moveFriction = 0.8
-    private val jumpHeight = -0.35
+    private val jumpHeight = -0.5
+    private val jumpExtraSpeed = 0.04
 
     private val runningLeft get() = input.keys.pressing(Key.A)
     private val runningRight get() = input.keys.pressing(Key.D)
     private val running get() = runningLeft || runningRight
     private val jumping get() = input.keys.justPressed(Key.SPACE) && cd.has(ON_GROUND_RECENTLY)
+    private val jumpingExtra get() = input.keys.justPressed(Key.SPACE) && cd.has(JUMP_EXTRA)
 
     private val fsm = stateMachine<HeroState>(HeroState.Idle) {
-        state(HeroState.Jump) {
+        state(HeroState.Fall) {
+            transition {
+                when {
+                    onGround -> HeroState.Idle
+                    else -> HeroState.Fall
+                }
+            }
+
             update {
+                run()
+            }
+        }
 
+        state(HeroState.Jump) {
+            transition {
+                when {
+                    velocityY > 0 -> HeroState.Fall
+                    else -> HeroState.Jump
+                }
+            }
 
+            begin {
+                if (jumping) {
+                    jump()
+                }
+            }
+
+            update {
+                if (jumpingExtra) {
+                    jumpExtra()
+                }
+                run()
             }
 
         }
@@ -118,6 +150,16 @@ class Hero(
         } else {
             velocityX *= moveFriction.pow(tmod)
         }
+    }
+
+    private fun jump() {
+        velocityY = jumpHeight
+        stretchX = 0.7
+        cd(JUMP_EXTRA, 100.milliseconds)
+    }
+
+    private fun jumpExtra() {
+        velocityY -= jumpExtraSpeed * tmod
     }
 
     override fun buildDebugInfo(container: UiContainer) {
