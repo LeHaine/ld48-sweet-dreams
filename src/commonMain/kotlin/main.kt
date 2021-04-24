ccb382a68f1aa1064972ed7c67d6ff2af6e71ba6
LeHaine <colt.daily@gmail.com>
2021-04-23 22:32:11 -0400

    adjust virtual size

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -14,8 +14,8 @@ suspend fun main() = Korge(Korge.Config(module = GameModule))
 object GameModule : Module() {
     override val mainScene = LevelScene::class
 
-    override val windowSize: SizeInt = SizeInt(Size(480 * 3, 270 * 3))
-    override val size: SizeInt = SizeInt(Size(480, 270))
+    override val windowSize: SizeInt = SizeInt(Size(320 * 4, 180 * 4))
+    override val size: SizeInt = SizeInt(Size(320, 180))
     override val bgcolor = Colors["#2b2b2b"]
 
     override suspend fun AsyncInjector.configure() {
