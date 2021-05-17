2e02ddd2493b6d61585c851c124a020650277744
LeHaine <colt.daily@gmail.com>
2021-05-17 09:44:44 -0400

    update korge to 2.1.1.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,15 @@ pluginManagement {
     plugins {
         id("com.soywiz.korge") version korgePluginVersion
     }
+
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.soywiz") {
+                useModule("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+            }
+        }
+    }
 }
 
 enableFeaturePreview("GRADLE_METADATA")
